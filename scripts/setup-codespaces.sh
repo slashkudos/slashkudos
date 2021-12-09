e0d80871783d21e59e497d423b7329eb05fb2947
Philip Gai <PhilipMGai@gmail.com>
2021-12-09 07:36:09 +0000

    Update github token using gh cli

--- scripts/setup-codespaces.sh
+++ scripts/setup-codespaces.sh
@@ -1,11 +1,18 @@
-#!/usr/bin/env pwsh
+#!/bin/bash
+
+unset GITHUB_TOKEN
 
 gh auth login -h github.com
+gh auth setup-git -h github.com
+gh auth status -t
 
 gh repo clone slashkudos/kudos-api /workspaces/kudos-api
-& /workspaces/kudos-api/scripts/Update-DevContainer.ps1
+pwsh /workspaces/kudos-api/scripts/Update-DevContainer.ps1
 
 gh repo clone slashkudos/kudos-site /workspaces/kudos-site
 gh repo clone slashkudos/kudos-test /workspaces/kudos-test
 gh repo clone slashkudos/kudos-twitter /workspaces/kudos-twitter
 gh repo clone slashkudos/kudos-web /workspaces/kudos-web
+
+GITHUB_TOKEN=$(sed -n -e 's/^.*oauth_token: //p' $HOME/.config/gh/hosts.yml)
+echo "GITHUB_TOKEN: $GITHUB_TOKEN"
