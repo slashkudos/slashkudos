5e715319eb09f19cd68b91af3bb8f1dd684a3d2d
Philip Gai <PhilipMGai@gmail.com>
2021-12-09 10:10:00 -0600

    Remove unset GITHUB_TOKEN
    This was causing the Codespaces to disappear 😱

--- scripts/setup-codespaces.sh
+++ scripts/setup-codespaces.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-unset GITHUB_TOKEN
-
 gh auth login -h github.com
 gh auth setup-git -h github.com
 gh auth status -t
-
-GITHUB_TOKEN=$(sed -n -e 's/^.*oauth_token: //p' $HOME/.config/gh/hosts.yml)
-echo "GITHUB_TOKEN: $GITHUB_TOKEN"
 
 gh repo clone slashkudos/kudos-api /workspaces/kudos-api
 pwsh /workspaces/kudos-api/scripts/Update-DevContainer.ps1
@@ -16,4 +11,3 @@ gh repo clone slashkudos/kudos-site /workspaces/kudos-site
 gh repo clone slashkudos/kudos-test /workspaces/kudos-test
 gh repo clone slashkudos/kudos-twitter /workspaces/kudos-twitter
 gh repo clone slashkudos/kudos-web /workspaces/kudos-web
-
