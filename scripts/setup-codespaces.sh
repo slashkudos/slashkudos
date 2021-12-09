17e8477ecc2765e313b42eb474ee45864ecde703
Philip Gai <PhilipMGai@gmail.com>
2021-12-09 07:58:24 +0000

    Set the github_token before cloning

--- scripts/setup-codespaces.sh
+++ scripts/setup-codespaces.sh
@@ -6,6 +6,9 @@ gh auth login -h github.com
 gh auth setup-git -h github.com
 gh auth status -t
 
+GITHUB_TOKEN=$(sed -n -e 's/^.*oauth_token: //p' $HOME/.config/gh/hosts.yml)
+echo "GITHUB_TOKEN: $GITHUB_TOKEN"
+
 gh repo clone slashkudos/kudos-api /workspaces/kudos-api
 pwsh /workspaces/kudos-api/scripts/Update-DevContainer.ps1
 
@@ -14,5 +17,3 @@ gh repo clone slashkudos/kudos-test /workspaces/kudos-test
 gh repo clone slashkudos/kudos-twitter /workspaces/kudos-twitter
 gh repo clone slashkudos/kudos-web /workspaces/kudos-web
 
-GITHUB_TOKEN=$(sed -n -e 's/^.*oauth_token: //p' $HOME/.config/gh/hosts.yml)
-echo "GITHUB_TOKEN: $GITHUB_TOKEN"
