9c48ecc36dd6f75d59a4187ab5b7ad1de3af2959
Philip Gai <PhilipMGai@gmail.com>
2021-12-19 05:25:16 +0000

    Reorganize setup-codespaces.sh
     - Added helpful comments
     - Clone all repos first, then run setup scripts

--- scripts/setup-codespaces.sh
+++ scripts/setup-codespaces.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Auth will use GITHUB_TOKEN from Codespace
 gh auth login -h github.com
 gh auth setup-git -h github.com
 gh auth status -t
 
+# Automatically clone public repos
+# GITHUB_TOKEN from Codespace cannot be used to clone private repos
 gh repo clone slashkudos/kudos-api /workspaces/kudos-api
-pwsh /workspaces/kudos-api/scripts/Update-DevContainer.ps1
-
 gh repo clone slashkudos/kudos-site /workspaces/kudos-site
-gh repo clone slashkudos/kudos-test /workspaces/kudos-test
 gh repo clone slashkudos/kudos-twitter /workspaces/kudos-twitter
 gh repo clone slashkudos/kudos-web /workspaces/kudos-web
+
+# Run repo specific setup scripts
+pwsh /workspaces/kudos-api/scripts/Update-DevContainer.ps1
