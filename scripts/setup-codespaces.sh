4111307904dbcc6af0ae245b93f33e2a64dd6aae
Philip Gai <PhilipMGai@gmail.com>
2021-12-28 20:10:38 +0000

    Handle missing SLASHKUDOS_PAT Update call to kudos-api setup script after rename

--- scripts/setup-codespaces.sh
+++ scripts/setup-codespaces.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-export GITHUB_TOKEN=$SLASHKUDOS_PAT
-echo 'export GITHUB_TOKEN=$SLASHKUDOS_PAT' >> ~/.bashrc
+if [ -z "$SLASHKUDOS_PAT" ]
+then
+    export GITHUB_TOKEN=$SLASHKUDOS_PAT
+    echo 'export GITHUB_TOKEN=$SLASHKUDOS_PAT' >> ~/.bashrc
+else
+    echo "WARNING: You must set Codespaces secret SLASHKUDOS_PAT"
+fi
 
-# Auth will use GITHUB_TOKEN from Codespace
+git config --global pull.rebase true
+
+# Auth will use GITHUB_TOKEN environment variable
 gh auth login -h github.com
 gh auth setup-git -h github.com
 gh auth status -t
 
 # Automatically clone public repos
-# GITHUB_TOKEN from Codespace cannot be used to clone private repos
+# GITHUB_TOKEN from Codespace cannot be used to clone private repos, must be PAT
 gh repo clone slashkudos/kudos-api /workspaces/kudos-api
 gh repo clone slashkudos/kudos-site /workspaces/kudos-site
 gh repo clone slashkudos/kudos-twitter /workspaces/kudos-twitter
 gh repo clone slashkudos/kudos-web /workspaces/kudos-web
 
 # Run repo specific setup scripts
-pwsh /workspaces/kudos-api/scripts/Update-DevContainer.ps1
+pwsh /workspaces/kudos-api/scripts/Setup-Codespaces.ps1
 
 # Install other dev tools
 brew install awscurl
